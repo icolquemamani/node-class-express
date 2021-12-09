293ac37b418551d78460315cb5b12c4404165471
Israel Colque Mamani <israelcolquemamani@externos-cl.cencosud.com>
2021-12-08 23:28:24 -0400

    Add tsc-watch script

--- src/routes/main.routes.ts
+++ src/routes/main.routes.ts
@@ -7,8 +7,12 @@ router.get('/', (req, res) => {
   res.send({message: 'Hello World!'});
 });
 
-router.get('/date', (req, res) => {
+router.get('/now', (req, res) => {
   res.send({now: new Date()});
+});
+
+router.get('/name', (req, res) => {
+  res.send({message: `My name is ${req.query.name}`});	
 });
 
 export default router;
